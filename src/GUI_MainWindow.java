5ac95327c60ced49a2cd3371028537959547e35a
IComplainInComments <optachan@outlook.com>
2024-02-28 19:33:35 -0500

    things

--- src/GUI_MainWindow.java
+++ src/GUI_MainWindow.java
@@ -1,6 +1,5 @@
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
