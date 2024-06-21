c8ec106e97c0d12aa61f83096a487727ec187bfa
IComplainInComments <optachan@outlook.com>
2024-06-21 17:21:48 -0400

    updated build scripts

--- build_app.sh
+++ build_app.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+NAME="JGAAP"
+VENDOR="EVLLabs"
+
+cd jgaap-app-build
+
 jpackage --input java-runtime \
     --type dmg \
     --app-version "9.0.0" \
-    --copy-right "\u00A9 2024 EVL lab" \
+    --copyright "\u00A9 2024 EVL lab" \
     --description "The Java Graphical Authorship Attribution Program" \
-    --name "JGAAP" \
-    --vendor "EVLLabs" \
-    --main-jar jgaap.jar \
-    --main-class com.jgaap.JGAAP \
-    --module-path /Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home/jmods \
-    --add-modules javafx.base,javafx.controls,javafx.graphics,java.base,java.desktop,java.logging,java.naming,java.rmi,java.xml \
+    --name $NAME \
+    --vendor $VENDOR \
+    --main-jar "JGAAP_JavaFX.jar" \
+    --main-class "com.jgaap.JGAAP" \
+    --module-path "/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home/jmods" \
+    --add-modules "javafx.base,javafx.controls,javafx.graphics,java.base,java.desktop,java.logging,java.naming,java.rmi,java.xml" \
+
+cp jgaap-app-build/java-runtime/JGAAP-9.0.0.dmg ../
 
