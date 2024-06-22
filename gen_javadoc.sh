20caed4529f1a81f6c38b2b1e75d97d1ee7b9284
IComplainInComments <optachan@outlook.com>
2024-06-22 17:11:34 -0400

    things.

--- gen_javadoc.sh
+++ gen_javadoc.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-javadoc --module-path=/Library/Java/Extensions/JavaFX_21/javafx-sdk-21.0.3/lib --add-modules=javafx.controls,javafx.fxml,javafx.graphics,javafx.base,javafx.swing -d Documentation/JavaDoc -sourcepath src GUI
+javadoc com.jgaap \
+ -private \
+ --module-path "/Library/Java/Extensions/JavaFX_21/javafx-sdk-21.0.3/lib" \
+ --add-modules=javafx.base,javafx.graphics,javafx.controls \
+ -d Documentation/JavaDoc \
+ -classpath "lib/external/" \
