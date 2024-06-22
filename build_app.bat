bcd39ba3e6440ac39fa64f1cab442a8f72fbf103
IComplainInComments <Optachan@outlook.com>
2024-06-22 02:35:07 -0400

    working windows build script

--- build_app.bat
+++ build_app.bat
@@ -1,14 +1,13 @@
-cd "C:\Program Files\Java\jdk-21\bin"
-.\jpackage --input "C:\Users\xana\Desktop\jpack\runtime" ^
+jpackage --input "jgaap-app-build\java-runtime" ^
 --type msi ^
 --app-version "9.0.0" ^
---copy-right "\u00A9 2024 EVL lab" ^
+--copyright "\u00A9 2024 EVL lab" ^
 --description "The Java Graphical Authorship Attribution Program" ^
 --name "JGAAP" ^
 --vendor "EVLLabs" ^
---main-jar "C:\Users\xana\Desktop\jpack\runtime\jgaap.jar" ^
+--main-jar "JGAAP_JavaFX.jar" ^
 --module-path "C:\Program Files\Java\jdk-21\bin" ^
 --add-modules javafx.base,javafx.controls,javafx.graphics,java.base,java.desktop,java.logging,java.naming,java.rmi,java.xml ^
--d C:\Users\xana\Desktop\jpack\runtime
+-d "%USERPROFILE%\Desktop"
 
 
