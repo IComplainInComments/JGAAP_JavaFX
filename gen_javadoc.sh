92dedc40145ea598fbd956fce8384a1527144103
IComplainInComments <optachan@outlook.com>
2024-07-02 13:58:11 -0400

    updates.

--- gen_javadoc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-javadoc com.jgaap \
- -private \
- --module-path "/Library/Java/Extensions/JavaFX_21/javafx-sdk-21.0.3/lib" \
- --add-modules=javafx.base,javafx.graphics,javafx.controls \
- -d Documentation/JavaDoc \
- -classpath "lib/external/" \
